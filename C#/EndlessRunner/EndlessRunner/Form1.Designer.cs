f739f1142c3830a9732dd36ad51f476f1fb1be1e
waninko <43968374+waninko@users.noreply.github.com>
2019-06-04 14:45:28 +0200

    Little game made in c#

---
+++ C#/EndlessRunner/EndlessRunner/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace EndlessRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.runner = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.runner)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.Black;
+            this.floor.Location = new System.Drawing.Point(-12, 452);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(652, 50);
+            this.floor.TabIndex = 0;
+            this.floor.TabStop = false;
+            // 
+            // runner
+            // 
+            this.runner.Image = global::EndlessRunner.Properties.Resources.running;
+            this.runner.Location = new System.Drawing.Point(131, 385);
+            this.runner.Name = "runner";
+            this.runner.Size = new System.Drawing.Size(44, 60);
+            this.runner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.runner.TabIndex = 1;
+            this.runner.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.Image = global::EndlessRunner.Properties.Resources.obstacle_1;
+            this.obstacle1.Location = new System.Drawing.Point(371, 405);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(23, 46);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.obstacle1.TabIndex = 2;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle";
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.Image = global::EndlessRunner.Properties.Resources.obstacle_2;
+            this.obstacle2.Location = new System.Drawing.Point(534, 400);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(50, 50);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle2.TabIndex = 3;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Tag = "obstacle";
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(13, 13);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(110, 29);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(616, 467);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.runner);
+            this.Controls.Add(this.floor);
+            this.Name = "Form1";
+            this.Text = "Endless Runner";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.runner)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox runner;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ C#/EndlessRunner/EndlessRunner/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EndlessRunner
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int jumpSpeed = 10;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rnd = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            resetGame();
+        }
+
+        private void gameEvent(object sender, EventArgs e)
+        {
+            //connecting jumpspeed with pictureboxes
+            runner.Top += jumpSpeed;
+
+            //show score in scoreText
+            scoreText.Text = "Score: " + score;
+
+            //if jumping is true and force less than 0, set jumping to false
+            if (jumping && force < 0)
+            {
+                jumping = false;
+            }
+
+            //if jumping is true change jumpSpeed to -12, reduce force
+            if (jumping)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                //else set jumpSpeed to 12
+                jumpSpeed = 12;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                //is X a pictureBox and an obstacle tag?
+                if (x is PictureBox && x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed; //move obst to the left
+
+                    if (x.Left + x.Width < -120)
+                    {
+                        //if obstacles has moved offscreen - respawn at far right w/random numbers
+                        x.Left = this.ClientSize.Width + rnd.Next(200, 800);
+                        //add to score
+                        score++;
+                    }
+                    //if collision with obstacle:
+                    if (runner.Bounds.IntersectsWith(x.Bounds))
+                    {   
+                        //the timer stops
+                        gameTimer.Stop();
+                        //change runner img to "dead" one
+                        runner.Image = Properties.Resources.dead;
+                        //show restart info
+                        scoreText.Text += "  Press R to restart  ";
+                    }
+                }
+            }
+            //if runner top is >= 380px and is not jumping
+            if (runner.Top >= 300 && !jumping)
+            {
+                force = 12;
+                runner.Top = floor.Top - runner.Height; //place player on top of pictureBox
+                jumpSpeed = 0;
+            }
+
+            if (score >= 10)
+            {
+                //speed up obstacles as score increases
+                obstacleSpeed = 15;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            //if space is pressed and jumping bool is false, then jumping is true.
+            if (e.KeyCode == Keys.Space && !jumping)
+            {
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            //if the R key is pressed & released - run the reset function
+            if (e.KeyCode == Keys.R)
+            {
+                resetGame();
+            }
+
+            //when key is released - check if jumping, and set to false if true
+            //so user can keep jumping
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+
+        public void resetGame()
+        {
+            force = 12;
+            runner.Top = floor.Top - runner.Height;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10;
+            scoreText.Text = "Score: " + score;
+            runner.Image = Properties.Resources.running;
+
+            foreach (Control x in this.Controls)
+            {
+                //is X a pictureBox and an obstacle?
+                if (x is PictureBox && x.Tag == "obstacle")
+                {
+                    //get random nr in position
+                    int position = rnd.Next(600, 1000);
+
+                    //change obstacles left position to random
+                    x.Left = 640 + (x.Left + position + x.Width * 3);
+                }
+            }
+            gameTimer.Start(); 
+        }
+    }
+}
